8d68c7b3843a7940fd9296b2b4a0350dd504abf3
pylsar <kysov2006@bk.ru>
2020-08-03 06:11:15 +0300

    delete from cart

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -12,6 +12,7 @@
 
 <script>
 import CartItem from './CartItem.vue'
+import {mapActions} from 'vuex'
 
 export default {
     name: 'Cart',
@@ -27,8 +28,11 @@ export default {
         }
     },
     methods:{
+        ...mapActions([
+            'DELETE_FROM_CART'
+        ]),
         deleteFromCart(index){
-            console.log(index)
+            this.DELETE_FROM_CART(index)
         }
     }
     
